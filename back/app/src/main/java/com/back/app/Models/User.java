8390f9c8ff8e03df970aa8e8fdc0d5c9383701c5
Rado23-04 <hei.rado.2@gmail.com>
2025-02-11 08:02:08 +0300

    fix: getter and setter

--- back/app/src/main/java/com/back/app/Models/User.java
+++ back/app/src/main/java/com/back/app/Models/User.java
@@ -1,10 +1,8 @@
 package com.back.app.Models;
 
 import jakarta.persistence.*;
-import lombok.*;
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
+
+
 @Table(name = "users")
 @Entity
 public class User {
@@ -15,6 +13,36 @@ public class User {
     @Column(nullable = false,unique = true)
     private String email;
 
-    @Column(nullable = false,unique = true)
-    private String passWord;
+    @Column(nullable = false)
+    private String password;
+
+    public User(String email, Long id, String password) {
+        this.email = email;
+        this.id = id;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
