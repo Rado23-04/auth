1f87363ac3d12e7eb60485bd4c53911811480019
Rado23-04 <hei.rado.2@gmail.com>
2025-02-12 15:13:50 +0300

    feat: add api for save user

--- front/src/services/authServices.tsx
+++ front/src/services/authServices.tsx
@@ -1,15 +1,28 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth";
-
-export const login = async (email: string, password: string) => {
+const API_URL = "http://localhost:8080/auth";
+export const login = async (email: string, password: string) => {    
     try {
-        const response = await axios.post(`${API_URL}/login`, {
+        const response = await axios.post(`${API_URL}`, {
             email,
             password
         });
-        return response.data;
+       return response.data;
     } catch (error) {
         throw error; 
     }
 };
+
+const API_URL_NEW_USER = "http://localhost:8080/save";
+export const saveUser = async (email:string,password:string)=>{
+    try {
+        const user = await axios.post(`${API_URL_NEW_USER}`,{
+            email,
+            password
+        });
+        return user.data;
+
+    } catch (error) {
+        throw error;
+    }
+}
