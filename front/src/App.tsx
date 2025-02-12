76717b8e60fd2a3e46c671b4b5afee26abff2631
Rado23-04 <hei.rado.2@gmail.com>
2025-02-12 15:11:58 +0300

    feat: add path /save

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
 import Signin from './components/Signin'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hello from './components/Hello';
+import NewUser from './components/NewUser';
 
 function App() {
 
   return (
     <>
-      <Signin/>
+      <Router>
+            <Routes>
+                <Route path="/" element={<Signin />} />
+                <Route path="/hello" element={<Hello />} />
+                <Route path='/save' element = {<NewUser/>}/>
+            </Routes>
+        </Router>
+
     </>
   )
 }
