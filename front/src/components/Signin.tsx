1f8da86efd9c42ee9bc0c28a1e441cec13bedbc1
Rado23-04 <hei.rado.2@gmail.com>
2025-02-12 15:13:07 +0300

    fix: navigate to page hello.tsx

--- front/src/components/Signin.tsx
+++ front/src/components/Signin.tsx
@@ -1,26 +1,33 @@
 import { useState } from "react"
 import { login } from "../services/authServices";
+import { useNavigate } from "react-router-dom";
 
 export default function Signin(){
 const [email,setEmail] = useState<string>("");
 const [password, setPassword] = useState<string>("");
+const navigate = useNavigate();
 
 const handleSubmit = async(e: any) =>{
     e.preventDefault();
     try {
         const data = await login(email,password)
             console.log("response serveur: ", data);
+            if(data.success){
+                navigate("/hello")
+            }else{
+                alert("Authentification failed")
+            }
             
     } catch (error) {
-        console.log("error", error);
-        
+        console.log("Erreur d'authentification:", error);
+        alert("Échec de l'authentification. Vérifiez vos identifiants.");
     }
     const newEmail = email;
     const newPassWord = password;
     
     setEmail("");
     setPassword("");
-}
+    }
 
     return <>
     <div className="container d-flew row">
