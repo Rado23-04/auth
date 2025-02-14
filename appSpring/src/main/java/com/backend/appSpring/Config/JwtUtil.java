85fb6101dc54ac7d302894dd377b03e289d59219
Rado23-04 <hei.rado.2@gmail.com>
2025-02-14 16:59:32 +0300

    feat: generated token

---
+++ appSpring/src/main/java/com/backend/appSpring/Config/JwtUtil.java
@@ -0,0 +1,53 @@
+package com.backend.appSpring.Config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String SECRET_KEY = "f93853221cd84ccab447afc619bc765a";
+
+    public SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean validateToken(String token, String email) {
+        try {
+            Claims claims = extractClaims(token);
+            return email.equals(claims.getSubject()) && !isTokenExpired(token);
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
