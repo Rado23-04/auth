ab344f3ec14d6abf28a5ab69f3f9f11a782630c6
Rado23-04 <hei.rado.2@gmail.com>
2025-02-15 15:24:56 +0300

    fix: reset database after each test

--- appSpring/src/test/java/com/backend/appSpring/ServicesTest/TestIntegrationServices/TestIntegrationUserService.java
+++ appSpring/src/test/java/com/backend/appSpring/ServicesTest/TestIntegrationServices/TestIntegrationUserService.java
@@ -3,10 +3,12 @@ package com.backend.appSpring.ServicesTest.TestIntegrationServices;
 import com.backend.appSpring.Models.User;
 import com.backend.appSpring.Repository.UserRepository;
 import com.backend.appSpring.Services.UserServices;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -17,7 +19,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-
 public class TestIntegrationUserService {
 
     @Autowired
@@ -25,6 +26,11 @@ public class TestIntegrationUserService {
 
     @Autowired
     private UserServices userServices;
+
+    @AfterEach
+    void cleanUp() {
+        userRepository.deleteAll();
+    }
 
     @Test
     void test_integration_user(){
